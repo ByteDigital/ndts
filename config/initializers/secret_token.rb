7d541937d0220cf8d985c376516dd5beb139ab70
Nigel Surtees <blanxfired@msn.com>
2015-02-09 20:55:16 +0000

    initial commit after restart

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ndts::Application.config.secret_token = '7b6dbbdadb58566ece2e2595ef0bd4b82627088ef28d2808b9c491c9822975cb85da7e2b534bb3ae8a42e5c60d8866b28585864786dd478ef7535bcff68dd822'
