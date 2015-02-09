120ebe6b90b3be46a72766772b63b0459abc37e4
Nigel Surtees <blanxfired@msn.com>
2015-02-09 19:14:55 +0000

    start again

--- cheaters/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Cheaters::Application.config.secret_token = '083dcb4000adc19851996b938c9cac9a0bd8f02e1f6a41d10869de45b5e7236247b242f6cfc3f1b3db84da9bf7a8a7fd4f4714ec1fa6697f87e88659653c1435'
