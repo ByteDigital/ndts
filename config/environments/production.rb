61771a3b71a095010b48f3bfe5917882562f1378
Nigel Surtees <blanxfired@msn.com>
2015-02-14 14:40:00 +0000

    assets again!

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ Ndts::Application.configure do
   config.serve_static_assets = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
