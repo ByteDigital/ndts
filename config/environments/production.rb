8b752444e0b2e218c21ea1ac0877b98ab8f1aec1
Nigel Surtees <blanxfired@msn.com>
2015-02-14 14:12:02 +0000

    assets again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,13 +8,11 @@ Ndts::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
-
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
-  config.assets.precompile += %w( public.js public.css  )
+  # Disable Rails's static asset server (Apache or nginx will already do this).
+  config.serve_static_assets = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
