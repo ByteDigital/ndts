55c5208632bf0a1bc9396234fcdadb8d7cb16f83
Nigel Surtees <blanxfired@msn.com>
2015-02-14 12:59:21 +0000

    fix bundle ruby issue

--- config/application.rb
+++ config/application.rb
@@ -59,7 +59,7 @@ module Ndts
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    ruby config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = false
 
   end
 end
